d0748ad2d68d1d09152fdc6ac7d39ff103df29a4
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2021-10-26 17:06:14 -0300

    caixa 1

---
+++ src/test/java/upe/br/mavenProjeto/ResultadoMegasenaTest.java
@@ -0,0 +1,19 @@
+package upe.br.mavenProjeto;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ResultadoMegasenaTest extends TestCase {
+	/** Número de dezenas esperadas no resultado da mega-sena. */
+	private final static int NUMERO_DE_DEZENAS = 6;
+	/**
+	* Teste do método obtemUltimoResultado()
+	*/
+	@Test
+	public void testObtemUltimoResultado() {
+		String[] ultimoResultado = ResultadoMegasena.obtemUltimoResultado();
+		assertNotNull(ultimoResultado);
+		assertTrue( ultimoResultado.length == NUMERO_DE_DEZENAS );
+	}
+}
