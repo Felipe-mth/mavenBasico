d35dc4dcd1eb53c879d58bb8e394f5d471a76651
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2021-10-26 11:31:16 -0300

    projeto iniciado com o arquétipo maven padrão

---
+++ src/main/java/upe/br/mavenProjeto/App.java
@@ -0,0 +1,13 @@
+package upe.br.mavenProjeto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
