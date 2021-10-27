23370d8946f7c8eb3d44e9aa7e75ea945b64a5d2
Felipe Matheus Costa <86928483+Felipe-mth@users.noreply.github.com>
2021-10-27 18:10:50 -0300

    Test funcionando

--- src/test/java/upe/br/mavenProjeto/ResultadoMegasenaTest.java
+++ src/test/java/upe/br/mavenProjeto/ResultadoMegasenaTest.java
@@ -7,7 +7,9 @@ import junit.framework.TestCase;
 
 public class ResultadoMegasenaTest extends TestCase {
 	/** Número de dezenas esperadas no resultado da mega-sena. */
-	private final static int NUMERO_DE_DEZENAS = 6;
+	private final static int DEZENAS_MEGASENA = 17;
+        private final static int DEZENAS_LOTO = 44;
+        private final static int DEZENAS_QUINA = 14;
 	/**
 	* Teste do método obtemUltimoResultado()
 	*/
@@ -15,6 +17,11 @@ public class ResultadoMegasenaTest extends TestCase {
 	public void testObtemUltimoResultado() {
 		Elements[] ultimoResultado = ResultadoMegasena.obtemUltimoResultado();
 		assertNotNull(ultimoResultado);
-		assertTrue( ultimoResultado.length == NUMERO_DE_DEZENAS );
+                
+            for (Elements ultimoResultado1 : ultimoResultado) {
+                assertTrue(ultimoResultado1.text().length() == DEZENAS_MEGASENA 
+                       || ultimoResultado1.text().length() == DEZENAS_LOTO 
+                       || ultimoResultado1.text().length() == DEZENAS_QUINA);
+            }
 	}
 }
